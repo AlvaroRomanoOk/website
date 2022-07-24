ffa05e3f4ec52f3faf7c8ec915e34110c0ea20c1
Alvaro Romano <alvaroromanook@gmail.com>
2022-07-24 13:56:44 -0300

    Add files via upload

---
+++ scripts/sliders.js
@@ -0,0 +1,43 @@
+const slider = document.querySelector("#slider");
+let sliderSection = document.querySelectorAll(".slider__section");
+let sliderSectionLast = sliderSection[sliderSection.length -1];
+
+const btnLeft = document.querySelector("#btn-left");
+const btnRight = document.querySelector("#btn-right");
+
+slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+
+function Next() {
+    let sliderSectionFrist = document.querySelectorAll(".slider__section")[0];
+    slider.style.marginleft = "-200%";
+    slider.style.transition = "all o.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('beforeend', sliderSectionFrist);
+        slider.style.marginleft = "-100%";
+    }, 500);
+}
+
+function Prev() {
+    let sliderSection = document.querySelectorAll(".slider__section");
+    let sliderSectionLast = sliderSection[sliderSection.length -1];
+    slider.style.marginleft = "0";
+    slider.style.transition = "all 0.5s";
+    setTimeout(function(){
+        slider.style.transition = "none";
+        slider.insertAdjacentElement('afterbegin', sliderSectionLast);
+        slider.style.marginleft = "-100%";
+    }, 500);
+}
+
+btnRight.addEventListener('click', function(){
+    Next();
+});
+
+btnLeft.addEventListener('click', function(){
+    Prev();
+});
+
+setInterval(function(){
+    Next();
+}, 5000);
