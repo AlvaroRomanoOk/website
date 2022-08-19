bf7e7751e0af8eb3b90a4cf7896fb93ab297a530
Alvaro Romano <alvaroromanook@gmail.com>
2022-08-18 22:00:31 -0300

    Add files via upload

---
+++ js/TextTyped1.js
@@ -0,0 +1,26 @@
+const typed = new Typed('.typed1', {
+    strings: [
+        '<i class="saludo" >HOLA</i>',
+        '<i class="saludo" >HELLO</i>',
+        '<i class="saludo" >CIAO</i>',
+        '<i class="saludo" >HALLO</i>',
+        '<i class="saludo" >SALUT</i>',
+        '<i class="saludo" >ПРИВЕТ</i>',
+		'<i class="saludo" >OLÁ</i>',
+		'<i class="saludo" >AHOJ</i>',
+		'<i class="saludo" >TERE</i>',
+		'<i class="saludo" >ALOHA</i>'
+    ],
+
+	typeSpeed: 75,
+	startDelay: 500,
+	backSpeed: 75,
+	smartBackspace: true,
+	shuffle: false,
+	backDelay: 1000,
+	loop: true,
+	loopCount: false,
+	showCursor: true,
+	cursorChar: '_',
+	contentType: 'html',
+});
