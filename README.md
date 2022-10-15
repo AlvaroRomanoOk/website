9bf32e77e23a4efe375c19477acfc5d6b3850d3f
Alvaro Romano <alvaroromanook@gmail.com>
2022-10-15 20:36:27 -0300

    reload

--- README.md
+++
@@ -1,7 +0,0 @@
-# Alvaro Romano | Sitio Web
-
-[![Netlify Status](https://api.netlify.com/api/v1/badges/ef3f6d28-a408-4022-933a-9d62692bac4b/deploy-status)](https://app.netlify.com/sites/alvaroromano/deploys)
-
-## Versión
-
-V: 1.0.0
--- js/TextTyped1.js
+++ js/TextTyped1.js
@@ -1,18 +1,12 @@
 const typed = new Typed('.typed1', {
     strings: [
-        '<i class="saludo" >HOLA</i>',
-        '<i class="saludo" >HELLO</i>',
-        '<i class="saludo" >CIAO</i>',
-        '<i class="saludo" >HALLO</i>',
-        '<i class="saludo" >SALUT</i>',
-        '<i class="saludo" >ПРИВЕТ</i>',
-		'<i class="saludo" >OLÁ</i>',
-		'<i class="saludo" >AHOJ</i>',
-		'<i class="saludo" >TERE</i>',
-		'<i class="saludo" >ALOHA</i>'
+        '<i class="saludo" >INFORMÁTICO</i>',
+        '<i class="saludo" >EDITOR AUDIOVISUAL</i>',
+        '<i class="saludo" >DESAROLLADOR WEB</i>',
+        '<i class="saludo" >INFORMÁTICO FORENSE</i>'
     ],
 
-	typeSpeed: 75,
+	typeSpeed: 100,
 	startDelay: 500,
 	backSpeed: 75,
 	smartBackspace: true,
