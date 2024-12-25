b9e2329f3cd460126e884c91e9e921a2f10774cf
Alvaro Romano <alvaroromanook@gmail.com>
2024-12-25 13:40:00 -0300

    reload

--- js/delete_anchor.js
+++ js/delete_anchor.js
@@ -1,11 +1,9 @@
-// Agrega un manejador al evento "onhashchange" de la ventana
-// Este evento se activa cuando el fragmento del hash en la URL cambia
-window.onhashchange = function () {
-    // Usa el método pushState para actualizar el historial del navegador
-    // Reemplaza el estado actual sin el fragmento del hash en la URL
+// Script para que en la url, sin importar el enlace, siempre se muestre "https://www.alvaroromano.com"
+
+window.onload = function () {
     window.history.pushState(
-        '',                   // Estado vacío: no se pasa información adicional al historial
-        document.title,       // Mantiene el título actual de la página
-        window.location.pathname // Define la URL sin incluir el fragmento del hash
+        '',
+        document.title,
+        'https://www.alvaroromano.com'
     );
-};
+}
