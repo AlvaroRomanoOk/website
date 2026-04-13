cc3c2d249d5eb4c1f4322fde807d91d155a88fb1
Alvaro Romano <alvaroromanook@gmail.com>
2026-04-12 23:53:32 -0300

    reload

--- script.js
+++ script.js
@@ -14,5 +14,27 @@ cerrar.addEventListener("click", () => {
 
 
 
+function cargarSeccion(archivo) {
+
+    document.querySelector('.nav').classList.remove('visible');
+
+    fetch(archivo)
+        .then(response => {
+            if (!response.ok) throw new Error("Error al cargar la sección");
+            return response.text();
+        })
+        .then(html => {
+            document.getElementById('content').innerHTML = html;
+        })
+        .catch(error => console.error(error));
+}
+
+window.onload = () => cargarSeccion('pages/inicio.html');
+
+
+
+
+
+
 
 
